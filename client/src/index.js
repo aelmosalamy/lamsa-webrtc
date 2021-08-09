d74572151b195f321486931860b31147357eb885
moha09-coder <mohamedelmossalamy@gmail.com>
2021-08-09 16:55:40 +0200

    Bottom bar fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import './index.css'
 import App from './App'
 
--- client/src/screens/MeetingScreen/components/BottomNavigation.js
+++ client/src/screens/MeetingScreen/components/BottomNavigation.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Dropdown, DropdownButton } from 'react-bootstrap';
 
 const BottomNavigation = ({ handleMute, handleDeafen, handleVideo }) => {
 	const [mutedSlash, setMutedSlash] = useState(false);
@@ -28,16 +28,26 @@ const BottomNavigation = ({ handleMute, handleDeafen, handleVideo }) => {
 					></i>
 				</div>
 			</div>
-			<button className="end_call">Leave Meeting</button>
+			<button className="end_call">Leave</button>
 			<div className="buttons">
-				<div className="button">
+				<div className="button" id="edit_button">
 					<i className={`fas fa-edit`}></i>
 				</div>
-				<div className="button">
+				<div className="button" id="eclipse_button">
 					<i className={`fas fa-expand`}></i>
 				</div>
-				<div className="button">
-					<i className={`fas fa-ellipsis-h`}></i>
+				<div className="dropup">
+					<div className="button dropdown-toggle" id="meeting_controls_dropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i className={`fas fa-ellipsis-h`}></i>
+					</div>
+					<div className="dropdown-menu meeting_controls_dropdown" aria-labelledby="meeting_controls_dropdown">
+						<div className="button dropdown-item">
+							<i className={`fas fa-edit`}></i>
+						</div>
+						<div className="button dropdown-item">
+							<i className={`fas fa-expand`}></i>
+						</div>
+					</div>
 				</div>
 			</div>
 			{/* </Container> */}
